f536a0f4fab72cdb2bc6511617e3278137a138a7
Carlo Askander <carloaskander@live.se>
2023-08-14 15:19:25 +0200

    Finished

---
+++ src/components/Input.jsx
@@ -0,0 +1,13 @@
+import './Input.css';
+
+const Input = ({setTodo, todo, addTodo}) => {
+    return (
+        <div className="input-wrapper">
+            <input type="text" className="Input" name="todo" placeholder="Create a todo" onChange={(event) => setTodo(event.target.value)} value={todo} />
+
+            <button className="add-button" onClick={addTodo}>Add</button>
+        </div>
+    );
+}
+ 
+export default Input;
